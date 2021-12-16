94b017ec2be541a9ee1cbd82ec7527df274df518
Josh <josh.tait3@gmail.com>
2021-12-16 21:41:36 +0000

    tweak seeding to demo the api a bit more

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PodcastSeeder::class,
-            EpisodeSeeder::class
+            EpisodeSeeder::class,
+            DownloadedEpisodeSeeder::class
         ]);
     }
 }
