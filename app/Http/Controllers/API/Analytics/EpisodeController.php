9ee917e8e2a8c2560fb35e59cf63ea3bcc515692
Josh <josh.tait3@gmail.com>
2021-12-16 19:08:35 +0000

    get basic analytics functionality working

--- app/Http/Controllers/API/Analytics/EpisodeController.php
+++ app/Http/Controllers/API/Analytics/EpisodeController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\API\Analytics;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\DownloadedEpisode;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Carbon\Carbon;
+
+use App\Models\Episode;
 
 /**
  * generic stat controller for episode resource
@@ -21,6 +27,114 @@ class EpisodeController extends Controller
     public function index()
     {
         //
+    }
+
+    /**
+     * Measures how many times a episode of a podcast
+     * has been downloaded over a given time period
+     *
+     * Tech test asks for last seven days so that's the default.
+     *
+     * @param Request $request
+     * @param Episode $episode
+     *
+     * @return Repsonse
+     *
+     */
+    public function downloads(Request $request, Episode $episode)
+    {
+        if ($episode->id === null) {
+            abort(404);
+        }
+
+        // setup some defaults in case the user hasn't provided any
+        // filter params
+        $defaults = [
+            'period' => 'days',
+            'interval' => 7
+        ];
+
+        /**
+         * Validation Rules:
+         * period:
+         *      must be one of 'daily', 'weekly' 'monthly' or 'yearly'
+         * interval:
+         *      must be a whole number between 1 and 365
+         *
+         * Both are optional since this is default
+         * period: 'daily',
+         * interval: 7
+         */
+
+        // lazily tack on any get params to the request body
+        $request->merge(
+            [
+                'period' => $request->get('period'),
+                'interval' => $request->get('interval')
+            ]
+        );
+
+        // create a custom validator to make validaing our request
+        // easier.
+        $validator = Validator::make($request->all(), [
+            'period' => [
+                Rule::in(['daily', 'weekly', 'monthly', 'yearly']),
+                'nullable'
+            ],
+            'interval' => ['digits_between:1,365', 'nullable']
+        ]);
+
+
+        // first up, perform some basic validation
+
+        if ($validator->fails()) {
+            return response(['error' => 'Invalid params'], 422);
+        }
+
+        $validated = $validator->validated();
+
+        $period = $validated['period'] ?? $defaults['period'];
+        $interval = $validated['interval'] ?? $defaults['interval'];
+
+
+        /**
+         * Now we can do the query, show your working: psuedomysql
+         * Select * from downloaded_episodes
+         * join podcast
+         * where episode_id = $episode->id
+         * and created_at within (interval, period)
+         * aggregate by day
+         */
+
+        // todays date minus 7 days
+        $ocurredAtDate = Carbon::now()->sub($interval, $period);
+
+        // start with the basic query so we can count how many there are in total
+        $downloadedEpisodes = DownloadedEpisode::with('podcast')
+            ->where('episode_id', $episode->id)
+            ->where('created_at', '>=', $ocurredAtDate)
+            ->orderBy('created_at')
+            ->get();
+
+        $count = count($downloadedEpisodes);
+
+        // continue the query by aggregating the data
+        // by day of week. No need to sort
+        $downloadedEpisodes = $downloadedEpisodes->groupBy(function($record) {
+
+                // using the short english day of week
+                // because it will be easier for frontend to create
+                // chart labels (less text, more to fit in a smaller area)
+                return Carbon::parse($record->created_at)
+                    ->shortEnglishDayOfWeek;
+            })->all();
+
+        return response([
+            'downloaded_episodes' => $downloadedEpisodes,
+            'filter_period'       => $period,
+            'filter_interval'     => $interval,
+            'total_count'         => $count
+        ]);
     }
 
     /**
